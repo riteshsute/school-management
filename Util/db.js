a9487aa4770e6f8418e240f91af296b88dd36f83
riteshsute <suteritesh@gmail.com>
2025-03-07 16:45:54 +0530

    db_update

--- Util/db.js
+++ Util/db.js
@@ -1,13 +1,14 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
-    database: "school-management", 
-    username: "root", 
-    password: "@Ritesh123",
-    host: "localhost",
+    database: process.env.DB_NAME, 
+    username: process.env.DB_USERNAME, 
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     dialectModule: require("mysql2"),
     benchmark: true
 });
+
 
 module.exports = sequelize;
--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const app = express();
-const dotenv = require("dotenv");
-
-dotenv.config();
+require('dotenv').config();
 
 const sequelize = require('./Util/db');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
+console.log(process.env.PORT);
+
 const school = require('./Models/school');
 const schoolRoute = require('./Routes/schoolRoute');
 
 app.use('/api', schoolRoute);
-app.use('/api1', schoolRoute)
+// app.use('/api1', schoolRoute)
 
 sequelize
   // .sync({ force: true })
   .sync() 
   .then((result) => {
     console.log("Database synced successfully");
-    app.listen(6000);  
+    app.listen(process.env.PORT);  
   })
   .catch((err) => {
     console.error("Error syncing database:", err); 
